9d1523a6b03d1247d2216063543bd8f3d7b07b69
Amalie S <amaliesmidth@gmail.com>
2018-08-23 14:59:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,24 @@
 def prime?(number)
   prime_array = []
+  return false if number % divisor == 0
+  end
+  true 
   
 end
 
-def is_prime n
-  for d in 2..(n - 1)
-   if (n % d) == 0
-    return false
-   end
-  end
+# def is_prime n
+#   for d in 2..(n - 1)
+#   if (n % d) == 0
+#     return false
+#   end
+#   end
 
-  true
- end
+#   true
+# end
 
 
 
-  (2...number).each do |divisor|
-    return false if number % divisor == 0
-  end
-  true
+#   (2...number).each do |divisor|
+    
+#   end
+#   true
