0d78a6d72e6c59876b312f6f43606f831ce2875d
Amalie S <amaliesmidth@gmail.com>
2018-08-23 15:14:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  if number >= 2
+    (2..number - 1).all? do |x|        # here it is checking whether our number is 
+      number % x != 0                 # divisible by any number between 2 and number-1 
+    end                               # by stating it cannot equal zero. if so, it is prime
+  else                                # (true) else, it is false (not prime)
+    return false
+  end
+end
