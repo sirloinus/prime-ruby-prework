db9cda3b7c6d8b201f13f92e055656a8ca1423cb
Amalie S <amaliesmidth@gmail.com>
2018-08-23 15:14:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,9 @@
-# def prime?(number)
-#   return false if number % 2 == 0
-#   true 
-# end
-
-
-
-
 def prime?(number)
   if number >= 2
-    (2..number - 1).all? do |x|
-      number % x != 0
-    end
-  else
+    (2..number - 1).all? do |x|        # here it is checking whether our number is 
+      number % x != 0                 # divisible by any number between 2 and number-1 
+    end                               # by stating it cannot equal zero. if so, it is prime
+  else                                # (true) else, it is false (not prime)
     return false
   end
 end
